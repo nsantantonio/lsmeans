92ac4206bb6383241d544ebe7eb5688471feef0c
Nicholas Santantonio <ns722@cornell.edu>
2021-01-13 11:18:14 -0500

    fixed function name, function

--- R/fitBlupH2.R
+++ R/fitBlupH2.R
@@ -1,24 +1,23 @@
 #' fitlsmeans function
 #'
-#' function to fit a linear model and calculate lsmeans, CV and LSD.
+#' function to fit a linear mixed model and calculate BLUPs and heritability.
 #'
-#' @param form object of class formula
+#' @param form object of class formula, compliant with lmer().
 #' @param data data.frame.  Name of effect for which lsmeans should be calculated
-#' @param meanType mean to use for calculating CV. Options include 'raw' = mean of y, 'effect' = mean of estimated effects, and 'intercept', the estimated intercept of th emodel fit. Default is "intercept". To reproduce CVs calculated by proc glm in SAS, use "intercept", or set contrasts to use sum contrsaints with 'options(contrasts = c("contr.sum", "contr.sum")'
-#' @param alpha numeric. Significance threshold. Default is 0.05
-#' @return list. Returns a list of BLUEs, mean used for CV, CV, and LSD. 
-#' @details 
-#' This function is a wrapper for lsmeans, and was made to calculate effects, CV and LSD as proc glm in SAS does. CVs are not reliable statistics for determining field trial quality.  
+#' @param addMu logical. Should the mean be added back into the BLUP? Default is FALSE
+#' @return list. Returns list of BLUP, mean, and h2, i.e. heritability
+#' @details extracts BlUPs and calculates heritability 
+#' This function is a wrapper for getBlupH2, and was made to calculate iid BLUPs and heritability 
 #' @examples
-#' fitlsmeans(mpg ~ factor(cyl), effect = "factor(cyl)", data = mtcars)
-#' fitlsmeans(Sepal.Length ~ Species, "Species", data = iris)
+#' fitBlupH2(mpg ~ 1|cyl, effect = "cyl", data = mtcars)
+#' fitBlupH2(Sepal.Length ~ 1|Species, "Species", data = iris)
 #' @export
-fitlsmeans <- function(form, effect, meanType = "intercept", alpha = 0.05, ...){
+fitBlupH2 <- function(form, effect, addMu = FALSE, ...){
 	require(lme4)
 	usrContr <- options("contrasts") # save user contrast options
 	options(contrasts = c('contr.sum', 'contr.sum'))
 	fit <- lmer(form, ...)
-	lsm <- getBlupH2(fit, effect = effect, meanType = meanType, alpha = alpha)
+	lsm <- getBlupH2(fit, effect = effect, addMu = addMu)
 	options(contrasts = usrContr$contrasts)
 	return(lsm)
 }
